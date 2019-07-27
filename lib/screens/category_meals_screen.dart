314ae877676e92c1760095718cf84e119b0f80fe
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-27 16:57:37 +0530

    Restructure and add meal item widget with image

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, Object>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          final meal = categoryMeals[index];
+
+          return MealItem(
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            complexity: meal.complexity,
+            affordability: meal.affordability,
+          );
+        },
+      ),
+    );
+  }
+}
